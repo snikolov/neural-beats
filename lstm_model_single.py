000160bc3c980704ac2a7f438e36f098a0f36c2a
snikolov <snikolov2@gmail.com>
2016-06-26 01:41:46 -0700

    Refactor data generation and sampling to not generate sequences across the boundaries of individual songs.

--- lstm_model_single.py
+++ lstm_model_single.py
@@ -16,7 +16,6 @@ from keras.optimizers import RMSprop
 import numpy as np
 
 from midi_util import array_to_midi, print_array
-from trials import trial_name
 
 np.random.seed(10)
 
@@ -35,33 +34,34 @@ MIN_HITS = 8
 ########################################################################
 # Network architecture parameters.
 ########################################################################
-NUM_HIDDEN_UNITS = 512
+NUM_HIDDEN_UNITS = 128
 # The length of the phrase from which the predict the next symbol.
-PHRASE_LEN = 32
+PHRASE_LEN = 64
 # Dimensionality of the symbol space.
 SYMBOL_DIM = 2 ** len(IN_PITCHES)
-NUM_ITERATIONS = 600
+NUM_ITERATIONS = 60
 BATCH_SIZE = 64
 
-VALIDATION_PERCENT = 0.01
+VALIDATION_PERCENT = 0.1
 
-#BASE_DIR = '/Users/snikolov/Dropbox/projects/neural-beats'
-BASE_DIR = '/home/ubuntu/neural-beats'
+BASE_DIR = '/Users/snikolov/Dropbox/projects/neural-beats'
+#BASE_DIR = '/home/ubuntu/neural-beats'
 
 #MIDI_IN_DIR = os.path.join(BASE_DIR, 'midi_arrays/mega/')
-MIDI_IN_DIR = os.path.join(BASE_DIR, 'midi_arrays/mega/Electronic Live 9 SD/Jungle')
+#MIDI_IN_DIR = os.path.join(BASE_DIR, 'midi_arrays/mega/Electronic Live 9 SD/Jungle')
+MIDI_IN_DIR = os.path.join(BASE_DIR, 'mega-pack/array/Electronic Live 9 SD/House')
 #MIDI_IN_DIR = os.path.join(BASE_DIR, 'midi_arrays/mega/Rock Essentials 2 Live 9 SD/Preview Files/Fills/4-4 Fills')
 
 #MIDI_IN_DIR = BASE_DIR + '/' + 'mega-pack/array/Rock Essentials 2 Live 9 SD/Preview Files/Fills/4-4 Fills'
 #MIDI_IN_DIR = '/Users/snikolov/Downloads/groove-monkee-midi-gm/array'
 
 MODEL_OUT_DIR = os.path.join(BASE_DIR, 'models')
-MODEL_NAME = '2016-06-22-5'
+MODEL_NAME = '2016-06-26-1'
 TRIAL_DIR = os.path.join(MODEL_OUT_DIR, MODEL_NAME)
 
 MIDI_OUT_DIR = os.path.join(TRIAL_DIR, 'gen-midi')
 
-LOAD_WEIGHTS = True
+LOAD_WEIGHTS = False
 
 
 # Encode each configuration of p pitches, each on or off, as a
@@ -79,7 +79,7 @@ decodings = {
 
 class SequenceDataGenerator:
     def __init__(self,
-                 sequence,
+                 sequences,
                  phrase_length=64,
                  batch_size=512,
                  validation_percent=0.01,
@@ -88,12 +88,13 @@ class SequenceDataGenerator:
 
         Arguments:
 
-        sequence - The symbolic, integer sequence to generate from
+        sequences - The list of symbolic, integer sequences
+            representing different "songs."
         phrase_length - The length of phrases to be generated
-        batch_size - The number of phrases to be generated.
+            batch_size - The number of phrases to be generated.
         '''
 
-        self.sequence = sequence
+        self.sequences = sequences
         self.phrase_length = phrase_length
         self.batch_size = batch_size
 
@@ -103,15 +104,20 @@ class SequenceDataGenerator:
         # indices.
         np.random.seed(0)
 
-        num_start_indices = len(sequence) - phrase_length
+        # Get the indices of all data points. A data point consists of
+        # a sequence of length phrase_length followed by the label, or
+        # next element in the sequence. Therefore, take all
+        # subsequences of length phrase_length + 1.
+        self.sequence_indices = idx_seq_of_length(self.sequences, phrase_length + 1)
+        n_points = len(self.sequence_indices)
         if is_validation:
-            self.allowed_indices = np.arange(num_start_indices)[
-                np.random.random(num_start_indices) < validation_percent]
+            self.data_indices = np.arange(n_points)[
+                np.random.random(n_points) < validation_percent]
         else:
-            self.allowed_indices = np.arange(num_start_indices)[
-                np.random.random(num_start_indices) >= validation_percent]
+            self.data_indices = np.arange(n_points)[
+                np.random.random(n_points) >= validation_percent]
 
-        assert len(self.allowed_indices) > 0, 'No data selected for {}'.format(is_validation)
+        assert len(self.data_indices) > 0, 'No data selected for {}'.format(is_validation)
 
 
     def gen(self):
@@ -126,12 +132,15 @@ class SequenceDataGenerator:
             y_batch = np.zeros((self.batch_size, SYMBOL_DIM))
 
             for batch_idx in xrange(self.batch_size):
-                phrase_start_idx = np.random.choice(self.allowed_indices)
+                # Choose a random data point. Then extract the
+                # sequence and label corresponding to it.
+                seq_idx, phrase_start_idx = self.sequence_indices[
+                    np.random.choice(self.data_indices)]
                 X_batch[batch_idx,
                         range(self.phrase_length),
-                        self.sequence[phrase_start_idx: phrase_start_idx + self.phrase_length]] = 1
+                        self.sequences[seq_idx][phrase_start_idx: phrase_start_idx + self.phrase_length]] = 1
                 y_batch[batch_idx,
-                        self.sequence[phrase_start_idx + self.phrase_length]] = 1
+                        self.sequences[seq_idx][phrase_start_idx + self.phrase_length]] = 1
 
             yield (X_batch, y_batch)
 
@@ -176,7 +185,7 @@ def prepare_data():
 
     # Sequence of configuration numbers representing combinations of
     # active pitches.
-    config_seq = []
+    config_sequences = []
     num_dirs = len([x for x in os.walk(MIDI_IN_DIR)])
     assert num_dirs > 0, 'No data found at {}'.format(MIDI_IN_DIR)
 
@@ -188,37 +197,37 @@ def prepare_data():
             array = np.load(os.path.join(root, filename))
             if np.sum(np.sum(array[:, in_pitch_indices]>0)) < MIN_HITS:
                 continue
-            config_seq.extend(encode(array[:, in_pitch_indices]))
+            config_sequences.append(np.array(encode(array[:, in_pitch_indices])))
         print 'Loaded {}/{} directories'.format(dir_idx + 1, num_dirs)
-    config_seq = np.array(config_seq)
 
     # Construct labeled examples.
-    num_examples = len(config_seq) - PHRASE_LEN
-
     # Use a generator for X and y as the whole dataset may not fit in
     # memory.
-    train_generator = SequenceDataGenerator(config_seq,
+    train_generator = SequenceDataGenerator(config_sequences,
                                             phrase_length=PHRASE_LEN,
                                             batch_size=BATCH_SIZE,
                                             is_validation=False,
                                             validation_percent=VALIDATION_PERCENT)
 
-    valid_generator = SequenceDataGenerator(config_seq,
+    valid_generator = SequenceDataGenerator(config_sequences,
                                             phrase_length=PHRASE_LEN,
                                             batch_size=BATCH_SIZE,
                                             is_validation=True,
-                                        validation_percent=VALIDATION_PERCENT)
-    '''
-    X = np.zeros((num_examples, PHRASE_LEN, SYMBOL_DIM), dtype=np.bool)
-    y = np.zeros((num_examples, SYMBOL_DIM), dtype=np.bool)
-    for i in xrange(num_examples):
-        for j, cid in enumerate(config_seq[i:i+PHRASE_LEN]):
-            X[i, j, cid] = 1
-        y[i, config_seq[i+PHRASE_LEN]] = 1
-    X = 1 * X
-    y = 1 * y
-    '''
-    return config_seq, train_generator, valid_generator
+                                            validation_percent=VALIDATION_PERCENT)
+
+    return config_sequences, train_generator, valid_generator
+
+
+def idx_seq_of_length(sequences, length):
+    '''List the start indices of all sequences of the given length. Each
+    start index is a pair of indices. The first specifies an element
+    of sequences and the second specifies the index within that
+    sequence.'''
+    indices = []
+    for i, seq in enumerate(sequences):
+        if len(seq) >= length:
+            indices.extend(itertools.product([i], range(len(seq) - length + 1)))
+    return indices
 
 
 def generate(model, seed, mid_name, temperature=1.0, length=512):
@@ -271,11 +280,12 @@ def init_model():
     return model
 
 
-def train(config_seq, train_generator, valid_generator):
+def train(config_sequences, train_generator, valid_generator):
     '''Train model and save weights.'''
 
     # Create the trial directory.
-    os.makedirs(TRIAL_DIR)
+    if not os.path.exists(TRIAL_DIR):
+        os.makedirs(TRIAL_DIR)
     # Copy the source file, with a version number, to the trial directory.
     source_filename = __file__
     versioned_source_filename = ''.join([
@@ -307,8 +317,11 @@ def train(config_seq, train_generator, valid_generator):
 
     best_val_loss = None
 
-    nb_val_samples = len(config_seq) * VALIDATION_PERCENT
-    print('Length of sequence: {}'.format(len(config_seq)))
+    sequence_indices = idx_seq_of_length(config_sequences, PHRASE_LEN + 1)
+    n_points = len(sequence_indices)
+
+    nb_val_samples = n_points * VALIDATION_PERCENT
+    print('Number of training points: {}'.format(n_points))
     print('Using {} validation batches'.format(nb_val_samples))
 
     for i in range(NUM_ITERATIONS):
@@ -332,7 +345,12 @@ def train(config_seq, train_generator, valid_generator):
             json.dump(history.history, fp)
             fp.write('\n')
 
-        start_index = np.random.randint(0, len(config_seq) - PHRASE_LEN - 1)
+        # Reset seed so we can compare generated patterns across iterations.
+        np.random.seed(0)
+
+        sequence_indices = idx_seq_of_length(config_sequences, PHRASE_LEN)
+        seq_index, phrase_start_index = sequence_indices[
+            np.random.choice(len(sequence_indices))]
         gen_length = 512
 
         # Generate samples.
@@ -340,10 +358,10 @@ def train(config_seq, train_generator, valid_generator):
             continue
 
         for temperature in [0.5, 0.75, 1.0]:
-            # Reset seed so we can compare generated patterns across iterations.
-            np.random.seed(0)
             generated = []
-            phrase = list(config_seq[start_index: start_index + PHRASE_LEN])
+            phrase = list(
+                config_sequences[seq_index][
+                    phrase_start_index: phrase_start_index + PHRASE_LEN])
 
             print('----- Generating with temperature:', temperature)
             phrase_array = decode(phrase)
@@ -357,8 +375,8 @@ def train(config_seq, train_generator, valid_generator):
 
 
 def run_train():
-    config_seq, train_generator, valid_generator = prepare_data()
-    train(config_seq, train_generator, valid_generator)
+    config_sequences, train_generator, valid_generator = prepare_data()
+    train(config_sequences, train_generator, valid_generator)
 
 def run_generate():
     print 'Loading model...'
@@ -366,36 +384,36 @@ def run_generate():
     model.load_weights(os.path.join(TRIAL_DIR, MODEL_NAME))
     seed = np.zeros((32, 6))
 
-    """
     # Normal techno pattern
-    seed[0,0] = 1 # Kick
+    seed[0,0] = 1 # kick
     seed[4,2] = 1 # hat
     seed[6,2] = 1 # hat
-    seed[8,0] = 1 # Kick
+    seed[8,0] = 1 # kick
     seed[12,2] = 1 # hat
-    seed[16,0] = 1 # Kick
+    seed[16,0] = 1 # kick
     seed[20,2] = 1 # hat
     seed[22,2] = 1 # hat
-    seed[24,0] = 1 # Kick
+    seed[24,0] = 1 # kick
     seed[28,2] = 1 # hat
-    """
 
+    """
     # Broken beat / electro pattern
     seed[0,0] = 1 # Kick
     seed[8,1] = 1 # Snare
     seed[12,0] = 1 # Kick
     seed[24,1] = 1 # Snare
     seed[30,1] = 1 # Snare
+    """
 
     print 'Generating...'
     length = 512
-    for temperature in [0.7,0.9,1.1]:
+    for temperature in [0.5,0.9,1.5,2]:
         print 'Temperature', temperature
-        for i in xrange(5):
+        for i in xrange(3):
             print 'i', i
             generate(model,
                      encode(seed),
-                     'jungle2_{}_{}_{}.mid'.format(length, temperature, i),
+                     'house_{}_{}_{}.mid'.format(length, temperature, i),
                      temperature=1.1,
                      length=length)
 
@@ -418,4 +436,4 @@ def run_generate():
                  length=length)
     """
 
-run_generate()
+run_train()
